cbd3476064d8f889d8681774478a59673b730b0b
zeruFili <zerubabelfilmon1@gmail.com>
2025-11-25 23:26:56 +0300

    added test for signup

---
+++ src/auth/auth.service.spec.ts
@@ -0,0 +1,82 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { getModelToken } from '@nestjs/mongoose';
+import mongoose, { Model } from 'mongoose';
+import { AuthService } from './auth.service';
+import { User } from './schemas/user.schema';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcryptjs';
+import { ConflictException, UnauthorizedException } from '@nestjs/common';
+
+describe('AuthService', () => {
+  let authService: AuthService;
+  let model: Model<User>;
+  let jwtService: JwtService;
+
+  const mockUser = {
+    _id: '61c0ccf11d7bf83d153d7c06',
+    name: 'Ghulam',
+    email: 'ghulam1@gmail.com',
+  };
+
+  let token = 'jwtToken';
+
+  const mockAuthService = {
+    create: jest.fn(),
+    findOne: jest.fn(),
+  };
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        JwtService,
+        {
+          provide: getModelToken(User.name),
+          useValue: mockAuthService,
+        },
+      ],
+    }).compile();
+
+    authService = module.get<AuthService>(AuthService);
+    model = module.get<Model<User>>(getModelToken(User.name));
+    jwtService = module.get<JwtService>(JwtService);
+  });
+
+  it('should be defined', () => {
+    expect(authService).toBeDefined();
+  });
+
+  describe('signUp', () => {
+    const signUpDto = {
+      name: 'Ghulam',
+      email: 'ghulam1@gmail.com',
+      password: '12345678',
+    };
+
+    it('should register the new user', async () => {
+      jest.spyOn(bcrypt, 'hash').mockResolvedValue('hashedPassword');
+      jest
+        .spyOn(model, 'create')
+        .mockImplementationOnce(() => Promise.resolve(mockUser));
+
+      jest.spyOn(jwtService, 'sign').mockReturnValue('jwtToken');
+
+      const result = await authService.signUp(signUpDto);
+
+      expect(bcrypt.hash).toHaveBeenCalled();
+      expect(result).toEqual({ token });
+    });
+
+    it('should throw duplicate email entered', async () => {
+      jest
+        .spyOn(model, 'create')
+        .mockImplementationOnce(() => Promise.reject({ code: 11000 }));
+
+      await expect(authService.signUp(signUpDto)).rejects.toThrow(
+        ConflictException,
+      );
+    });
+  });
+
+  
+});
